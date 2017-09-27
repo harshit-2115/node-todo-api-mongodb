2dc9639cec06c5d6fb72646c7fcf7c8395c6f730
Harshit <harshit.2115@gmail.com>
2017-09-27 11:13:57 +0530

    delete-file

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,24 @@
+ // const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB Server');
+	}
+	console.log('Connected to MongoDB Server');
+
+	// db.collection('Todos').deleteMany({text: 'Hello World'}).then((result) => {
+	// 	console.log('Deleted');
+	// });
+
+	// db.collection('Todos').deleteOne({text: 'Something else to do'}).then((result) => {
+	// 	console.log('Deleted');
+	// });
+	
+	db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+		console.log(result);
+	});
+
+
+	// db.close();
+});
